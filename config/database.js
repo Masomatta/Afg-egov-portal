25f4c5d2e95c95ffe46a33b7b37c045436983e41
Masooma Atayee <masooma.atayee222@gmail.com>
2025-09-28 09:24:27 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,20 @@
+
 import { Pool } from "pg";
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+
+dotenv.config(); 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // required on Render
+  connectionString: process.env.DATABASE_URL, 
+  ssl: { rejectUnauthorized: false } 
 });
+
+
+pool.connect()
+  .then(client => {
+    console.log("✅ Connected to PostgreSQL on Render");
+    client.release();
+  })
+  .catch(err => console.error("❌ Database connection error:", err.stack));
 
 export default pool;
