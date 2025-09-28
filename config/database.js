88289f31d1afd8fc762f74a02ad137a6dc6ea0fc
Masooma Atayee <masooma.atayee222@gmail.com>
2025-09-28 09:16:48 +0800

    databaseurl updated

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
-import {Pool} from "pg"
-import dotenv from "dotenv"
-dotenv.config("./env")
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
- const pool = new Pool({
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required on Render
 });
+
 export default pool;
--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-//import app from "./app.js";
-//import dotenv from "dotenv";
-//dotenv.config("/.env");
 
-//const testDb = async () => {
-//    console.log("Database is connected.");
-//};
-//testDb();
-//app.listen(process.env.PORT, () => {
-//  console.log(`The server is on, listening on port ${process.env.PORT}!`);
-//});
-
-// src/index.js
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
